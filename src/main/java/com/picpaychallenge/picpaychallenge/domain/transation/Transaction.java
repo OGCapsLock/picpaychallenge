c0aeb1c52d4558f9f2231cfeb0c462274a2af5ec
Osvaldo Amosse Cuambe <osvaldoamosse@gmail.com>
2023-09-19 13:38:41 +0200

    Add User clas

---
+++ src/main/java/com/picpaychallenge/picpaychallenge/domain/transation/Transaction.java
@@ -0,0 +1,34 @@
+package com.picpaychallenge.picpaychallenge.domain.transation;
+
+import com.picpaychallenge.picpaychallenge.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name="transactions")
+@Table(name="transaction")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amout;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+}
